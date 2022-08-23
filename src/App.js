a75b1056a056c9830d5ee395b558c9123cd1e4dd
gabrielvoltani <gabriel.voltani@gmail.com>
2022-08-23 17:20:26 -0300

    R1 ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunesss</p>);
+    return (
+      <Switch>
+        <Route exact path="/" render={ () => <Login /> } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="" component={ NotFound } />
+        {/* <Route exact="false" path="" component={ NotFound } /> */}
+        {/* ver se o certo é o que ta ou o que não ta */}
+      </Switch>
+    );
   }
 }
 
